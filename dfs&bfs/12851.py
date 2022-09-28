6d792421b5970a137c847bb6a92bb680ed35133a
jungeunevepark <evecomcom98@naver.com>
2022-09-28 19:01:05 +0900

    더 짧게 수정

--- dfs&bfs/12851.py
+++ dfs&bfs/12851.py
@@ -3,12 +3,17 @@ from collections import deque, defaultdict
 N, M = map(int, input().split())
 q = deque([(N, 0)])
 already = defaultdict(int)
+count = 0
+result = 100000                 # 0 100000의 입력이 들어와도 100000초 내에는 만날수 있음
 
 while q:
     v, time = q.popleft()
+    if time > result:
+        continue
     if v == M:
-        result = time
-        break
+        if time <= result:
+            result = time
+            count += 1
     if v - 1 >= -1:
         if already[v-1] == 0 or already[v-1] >= time+1:
             already[v-1] = time+1
@@ -23,26 +28,4 @@ while q:
             q.append((2*v, time + 1))
 
 print(result)
-count = 1
-while q:
-    v, time = q.popleft()
-    if time > result:
-        continue
-    if v == M:
-        if result == time:
-            count += 1
-    if not (v == M + 1 or v == M - 1 or v == (1/2)*M):
-        continue
-    if v - 1 >= -1:
-        if already[v-1] == 0 or already[v-1] >= time+1:
-            already[v-1] = time+1
-            q.append((v-1, time + 1))
-    if v + 1 <= M+1:
-        if already[v+1] or already[v+1] >= time+1:
-            already[v+1] = time + 1
-            q.append((v+1, time + 1))
-    if 2*v <= 2*M:
-        if already[2*v] or already[v-1] >= time+1:
-            already[2*v] = time+1
-            q.append((2*v, time + 1))
 print(count)
