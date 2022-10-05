180048c15374807132803c49006e38b679b2e5e8
jungeunevepark <evecomcom98@naver.com>
2022-10-05 19:39:33 +0900

    success

---
+++ DP/2579.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open("input.txt", "r")
+N = int(input())
+step = [0] + [int(input()) for _ in range(N)]
+
+d = [[0] * 2 for _ in range(N+1)]
+d[1] = [step[1], 0]
+
+for i in range(2, N+1):
+    d[i][0] = max(d[i-2][0], d[i-2][1]) + step[i]   # 바로 전 계단을 안 밟는 경우
+    d[i][1] = d[i-1][0] + step[i]     # 밟는 경우
+print(max(d[N][0], d[N][1]))
