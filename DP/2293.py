1e8e7b75fbb9d4c2ee29908e09abc6286c8809cd
jungeunevepark <evecomcom98@naver.com>
2022-10-07 15:00:42 +0900

    fail

--- DP/2293.py
+++ DP/2293.py
@@ -1,8 +1,12 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
 n, k = map(int, input().split())
 array = list(int(input()) for _ in range(n))
-d = [0] * (k+1)  # 가치가 k일 때 가능한 경우의 수
-d[min(array)] = 1
-for i in range(min(array) + 1, k+1):
-    for j in array:
-        if i > j:
-            d[i] += d[i-j]
+d = [0] * (k+1)
+d[0] = 1
+
+for coin in array:
+    for value in range(coin, k+1):
+        d[value] += d[value-coin]
+print(d[k])
