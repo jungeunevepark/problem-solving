c00d7fdabdfd187ff57cb960f5afd3f81b8b3b92
jungeunevepark <evecomcom98@naver.com>
2022-10-07 11:10:51 +0900

    fail

--- DP/7579.py
+++ DP/7579.py
@@ -2,13 +2,20 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 N, M = map(int, input().split())
-app = list(map(int, input().split()))
-cost = list(map(int, input().split()))
-d = [[] for _ in range(N)]
-result = 1e6
+app = [0] + list(map(int, input().split()))
+cost = [0] + list(map(int, input().split()))
+d = [[0]*(sum(cost)+1) for _ in range(N+1)]
+result = sum(cost)
 
-
-for idx, memory in enumerate(app):
-    d[idx][0] = sum(d[idx-1])     # 전에 것과 더하는 경우
-
-print(result)
+for i in range(1, N+1):
+    for j in range(1, sum(cost)+1):
+        if j >= cost[i]:
+            d[i][j] = max(d[i-1][j], d[i-1][j-cost[i]]+app[i])
+        else:
+            d[i][j] = d[i-1][j]
+        if d[i][j] >= M:
+            result = min(result, j)
+if M != 0:
+    print(result)
+else:
+    print(0)
