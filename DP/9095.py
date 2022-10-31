3857b884f7cbffa0ed90fd653941fa2d4f43bfc0
jungeunevepark <evecomcom98@naver.com>
2022-10-31 16:38:42 +0900

    success

---
+++ DP/9095.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("input.txt", "r")
+for _ in range(int(input())):
+    n = int(input())
+    d = [1, 1, 2] + [0] * (n-2)
+    for i in range(3, n+1):
+        d[i] = d[i-1] + d[i-2] + d[i-3]
+    print(d[n])
